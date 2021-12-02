817d604d7c03df2abacc32dc0139fffba2665d1f
Robert Dunmire III <slackr31337@gmail.com>
2021-12-01 21:47:43 -0500

    Update patch_dmidecode.sh

--- scripts/patch_dmidecode.sh
+++ scripts/patch_dmidecode.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-VER=$(python -c 'import sys; print(str(sys.version_info[0])+"."+str(sys.version_info[1]))')
 . ./scripts/envvars.sh
-# cd ${HOMEAGENT}
+cd ${HOMEAGENT}
+VER=$(python3 -c 'import sys; print(str(sys.version_info[0])+"."+str(sys.version_info[1]))')
 FILE=env/lib/python${VER}/site-packages/dmidecode.py
+
 sed -i 's:subprocess.Popen(self.dmidecode:subprocess.Popen(["/usr/bin/sudo", self.dmidecode]:' $FILE
