4bbae5388b6cfe2f31a8f58041b1b87d382a5efd
Robert Dunmire III <slackr31337@gmail.com>
2022-08-30 03:52:38 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,7 +7,7 @@ export HOMEAGENT=${DIR}
 echo "Intalling Debian 11 dependency packages"
 
 sudo apt update
-sudo apt install -y python3 python3-virtualenv dmidecode
+sudo apt install -y python3 python3-virtualenv python3-venv dmidecode
 
 ${PY} -V
 
@@ -16,13 +16,12 @@ sudo apt install -y libglib2.0-dev python3-dbus python3-gi python3-gi-cairo
 
 # Needed for bluetooth sensors
 #sudo apt install -y rfkill bluez bluez-firmware bluez-hcidump bluez-tools
-sudo apt-get install bluetooth bluez libbluetooth-dev libudev-dev
+sudo apt install bluetooth bluez libbluetooth-dev libudev-dev
 
 echo "Adding homeagent user"
 sudo adduser --system --home ${DIR} --no-create-home --disabled-login homeagent
 sudo addgroup homeagent
 sudo usermod -aG bluetooth homeagent
-#sudo usermod -aG sudo homeagent
 mkdir /home/homeagent
 chmod 770 /home/homeagent
 
